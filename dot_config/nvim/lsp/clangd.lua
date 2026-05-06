a0cbf737dfb9610610630c8457ee15be755e7924
Simon Koopmans <s.koopmans@acs-buildings.com>
2026-05-06 19:02:50 +0200

    Remove nvim local garbage and setup external garbage tracking for nvim

--- dot_config/nvim/lsp/clangd.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { "clangd", "--compile-commands-dir=." },
-  root_markers = { "compile_commands.json", ".git" },
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-}
--- dot_config/nvim/lua/plugins/dotted-lines.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {
-    indent = { char = "┊" }, -- or "│", "¦", "⎸", "▏"
-    scope = { enabled = true }, -- highlights active scope (matching {})
-  },
-}
